445efe8dc8891e029d500fae67949f7506b01b74
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-01 17:10:02 -0600

    score

--- project-3/src/components/TotalScore.jsx
+++ project-3/src/components/TotalScore.jsx
@@ -12,8 +12,12 @@ const TotalScore = () => {
 export default TotalScore;
 
 const ScoreContainer = styled.div`
+    max-width: 200px;
+    text-align: center;
+    text-align: center;
     h1 {
         font-size: 100px;
+        line-height: 100px;
     }
     p {
         font-size: 24px;
