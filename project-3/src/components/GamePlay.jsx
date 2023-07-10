e8f1088d4ec82c72942323cac2a0d29dfe9c8d24
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-09 18:46:17 -0600

    gameplay

--- project-3/src/components/GamePlay.jsx
+++ project-3/src/components/GamePlay.jsx
@@ -1,13 +1,25 @@
+import { styled } from "styled-components";
 import NumberSelector from "./NumberSelector";
 import TotalScore from "./TotalScore"
 
 const GamePlay = () => {
   return (
-    <main>
+    <MainContainer>
+        <div className="top_section">
         <TotalScore />
         <NumberSelector/>
-    </main>
+        </RoleDice>
+    </MainContainer>
   )
 }
 
 export default GamePlay;
+
+const MainContainer = styled.main`
+  padding-top: 70px;
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+`;
