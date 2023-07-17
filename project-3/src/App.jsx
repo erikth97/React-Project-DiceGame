8d4af3546912edbdf3430ecd8803a6b3775d104e
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-17 14:28:21 -0600

    App

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-import StartGame from "./components/startGame";
+import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
 
 function App() {
-  const [isGameStarted, setIsGameStarted] = useState(true);
+  const [isGameStarted, setIsGameStarted] = useState(false);
 
   const toggleGamePlay = () => {
     setIsGameStarted((prev) => !prev);
-  }
+  };
 
   return (
-  <> {isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
   );
 }
 
-export default App
-
+export default App;
