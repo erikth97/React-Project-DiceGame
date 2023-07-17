266e45a38c6242ee2318c47f00c9b53150e0bb71
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-17 14:28:39 -0600

    role

--- project-3/src/components/RoleDice.jsx
+++ project-3/src/components/RoleDice.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import styled from "styled-components";
 
-function RoleDice() {
-  return (
-    <div>RoleDice</div>
-  )
-}
+const RoleDice = ({  roleDice, currentDice }) => {
+    return (
+    <DiceContainer>
+      <div className="dice" onClick={roleDice}>
+        <img src={`/images/dice/dice_${currentDice}.png`} alt="dice 1" />
+      </div>
+        <p>Click on Dice to roll</p>
+    </DiceContainer>
+    )
+  };
 
-export default RoleDice
+export default RoleDice;
+
+  const DiceContainer = styled.div`
+      margin-top: 48px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+  .dice {
+    cursor: pointer;
+  }    
+
+  p {
+      font-size: 24px;
+  }
+`;
