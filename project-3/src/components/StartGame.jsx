5e0b6819f7bd3cfc224ba2a2d1fa1eb02d22cd14
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-17 14:28:53 -0600

    startcomponent

--- project-3/src/components/StartGame.jsx
+++ project-3/src/components/StartGame.jsx
@@ -1,17 +1,18 @@
 import styled from "styled-components";
+import { Button } from "../styled/Button";
 
 const StartGame = ({ toggle }) => {
   return (
-  <Container>
-    <div>
-    <img src="/images/dices.png" />
-    </div>
-    <div className="content">
-      <h1>DiceGame</h1>
-      <Button onClick={toggle}>Play Now</Button>
-    </div>
-  </Container>
-  )
+    <Container>
+      <div>
+        <img src="/images/dices.png" />
+      </div>
+      <div className="content">
+        <h1>Dice Game</h1>
+        <Button onClick={toggle}>Play Now</Button>
+      </div>
+    </Container>
+  );
 };
 
 export default StartGame;
@@ -25,27 +26,8 @@ const Container = styled.div`
 
   .content {
     h1 {
-    font-size: 96px;
-    white-space: nowrap;
+      font-size: 96px;
+      white-space: nowrap;
     }
   }
 `;
-const Button = styled.button`
-    color: white;
-    padding: 10px 18px;
-    background: #000000;
-    border-radius: 5px;
-    min-width: 220px;
-    border: none;
-    font-size: 16px;
-    border: 1px solid transparent; 
-    cursor: pointer;
-    transition: 0.3s background ease-in;
-    &:hover{
-      background-color: white;
-      border: 1px solid black;
-      color: black;
-      transition: 0.3s background ease-in;
-    }
-    `;
-
