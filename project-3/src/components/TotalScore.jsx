64e91c29bf23e4255ead202cad1d33abf146dbf4
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-07-17 14:29:05 -0600

    scorecomponent

--- project-3/src/components/TotalScore.jsx
+++ project-3/src/components/TotalScore.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const TotalScore = () => {
+const TotalScore = ({ score }) => {
     return (
         <ScoreContainer>
-            <h1>0</h1>
+            <h1>{score}</h1>
             <p>total Score</p>
         </ScoreContainer>
     )
@@ -13,7 +13,6 @@ export default TotalScore;
 
 const ScoreContainer = styled.div`
     max-width: 200px;
-    text-align: center;
     text-align: center;
     h1 {
         font-size: 100px;
